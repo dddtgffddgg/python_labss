954311888ea9b54724ff60d2259b922b9490ba93
dddtgffddgg <115697740+dddtgffddgg@users.noreply.github.com>
2023-02-15 19:49:36 +0500

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,6 +1,11 @@
-mod = 10
+mod = 10  #Способ 1
 for i in range(1, 1001): 
     if i == mod:
         mod *= 10 
     if (i*i) % mod == i:
-        print(i)    
+        print(i)
+
+for i in range(1, 1001):  #Способ 2
+    res = len(str(i))
+    if i == (i**2) % 10 ** res:
+        print(i)   
