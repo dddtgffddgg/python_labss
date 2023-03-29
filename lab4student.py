2dd46829940cff256f1fe4b64795a7089e9083aa
dddtgffddgg <115697740+dddtgffddgg@users.noreply.github.com>
2023-03-30 00:09:21 +0500

    Update lab4student.py

--- lab4student.py
+++ lab4student.py
@@ -1,14 +1,23 @@
-educational_grant, expenses = 10000, 12000
-i = 0
-month_expenses = 0
-money = 0
-need = educational_grant
-while i < 10: 
-    if i == 0:
-        month_expenses = expenses
-    elif i >= 1: 
-        month_expenses *= 1.03
-    i += i
-    diference = round(month_expenses-need, 2)
-    money += diference 
-print('Studentu nado poprosit', money, 'rub')
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# (цикл while)
+
+# Ежемесячная стипендия студента составляет educational_grant руб., а расходы на проживание превышают стипендию
+# и составляют expenses руб. в месяц. Рост цен ежемесячно увеличивает расходы на 3%, кроме первого месяца
+# Составьте программу расчета суммы денег, которую необходимо единовременно попросить у родителей,
+# чтобы можно было прожить учебный год (10 месяцев), используя только эти деньги и стипендию.
+# Формат вывода:
+#   Студенту надо попросить ХХХ.ХХ рублей
+
+educational_grant, expenses = 10000, 12000 #стипендия, расходы за проживание
+
+# TODO здесь ваш код
+expenses_in_month, expenses_all = expenses, expenses
+i = 1
+while i < 10:
+    expenses_in_month = expenses_in_month * 1.03
+    expenses_all = expenses_all + expenses_in_month
+    i += 1
+result = expenses_all - educational_grant * 10
+print('Студенту надо попросить', result, 'рублей.')
