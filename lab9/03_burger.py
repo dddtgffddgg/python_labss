7a3040a2b0815b70bff65cfc512f395116a1e234
dddtgffddgg <115697740+dddtgffddgg@users.noreply.github.com>
2023-05-04 00:46:29 +0500

    Update 03_burger.py

--- lab9/03_burger.py
+++ lab9/03_burger.py
@@ -15,62 +15,38 @@
 
 # Создать рецепт своего бургера, по вашему вкусу.
 # Если не хватает ингридиентов - создать соответствующие функции в модуле my_burger
-# my_burger.py
-
-def add_bun():
-    print("А теперь добавим булочки")
-
-def add_patty():
-    print("А теперь добавим котлеты")
-
-def add_pickle():
-    print("А теперь добавим огурчика")
-
-def add_ketchup():
-    print("А теперь добавим кетчупа")
-
-def add_mustard():
-    print("А теперь добавим горчицы")
-
-def add_cheese():
-    print("А теперь добавим сыра")
-
-def add_tomato():
-    print("А теперь добавим помидорку")
-
-def add_special_sauce():
-    print("А теперь добавим специальный соус")
-
-def add_salat():
-    print("А теперь добавим салата")            
+# my_burger.py      
 
 # example.py
 
 import my_burger
-print("Для приготовления двойного чизбургера нужно:")
 
-my_burger.add_bun()
-my_burger.add_patty()
-my_burger.add_cheese()
-my_burger.add_patty()
-my_burger.add_cheese()
-my_burger.add_pickle()
-my_burger.add_ketchup()
-my_burger.add_mustard()
-my_burger.add_bun()
+print("Для приготовления двойного чизбургера нужно:")
+def double_cheeseburger():
+    my_burger.add_bun()
+    my_burger.add_patty()
+    my_burger.add_cheese()
+    my_burger.add_patty()
+    my_burger.add_cheese()
+    my_burger.add_pickle()
+    my_burger.add_ketchup()
+    my_burger.add_mustard()
+    my_burger.add_bun()
+double_cheeseburger()    
 
 print("Готово! Приятного аппетита!")
 
 import my_burger
 print("Для приготовления бургера Гранд Де Люкс нам нужно:")
-my_burger.add_bun()
-my_burger.add_ketchup()
-my_burger.add_mustard()
-my_burger.add_pickle()
-my_burger.add_salat()
-my_burger.add_tomato()
-my_burger.add_cheese()
-my_burger.add_patty()
-my_burger.add_cheese()
-my_burger.add_bun()
+def burger_Grand_De_Lyux():
+    my_burger.add_bun()
+    my_burger.add_ketchup()
+    my_burger.add_mustard()
+    my_burger.add_pickle()
+    my_burger.add_salat()
+    my_burger.add_tomato()
+    my_burger.add_cheese()
+    my_burger.add_patty()
+    my_burger.add_cheese()
+    my_burger.add_bun()
 print("Готово! Приятного аппетита!")
