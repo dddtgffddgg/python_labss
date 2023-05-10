8ddb7d84187310cc19924b129bd9208d9efca68c
dddtgffddgg <115697740+dddtgffddgg@users.noreply.github.com>
2023-05-10 22:51:45 +0500

    Update trees.py

--- lab9/drawing/trees.py
+++ lab9/drawing/trees.py
@@ -1,20 +1,17 @@
-# -*- coding: utf-8 -*-
-
 import simple_draw as sd
 import random
 
 def draw_tree():
     sd.resolution = (1200, 600)
     point_0 = sd.get_point(600, 0)
-    v0 = sd.get_vector(point_0, angle=90, length=50)
-    v0.draw()
+    sd.vector(start=point_0, angle=90, length=50).draw()
 
     def branch(point, angle, length):
         if length < 5:
             return
-        v1 = sd.get_vector(start_point=point, angle=angle, length=length)
+        v1 = sd.vector(start=point, angle=angle, length=length)
         v1.draw()
-        v2 = sd.get_vector(start_point=point, angle=angle, length=length)
+        v2 = sd.vector(start=point, angle=angle, length=length)
         v2.draw()
         next_point1 = v1.end_point
         next_point2 = v2.end_point
@@ -22,6 +19,5 @@ def draw_tree():
         branch(point=next_point1, angle=angle-random.uniform(18, 42), length=next_length)
         branch(point=next_point2, angle=angle+random.uniform(18, 42), length=next_length)
 
-    branch(point=v0.end_point,angle=90, length=100)
+    branch(point=point_0.end_point, angle=90, length=100)
     sd.pause()
-////здесь должна скачать pip install Pillow
