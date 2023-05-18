9c8309a6b3fefafd03f1854e4c73c096271ff6e3
dddtgffddgg <115697740+dddtgffddgg@users.noreply.github.com>
2023-05-18 11:01:00 +0500

    Update 1_snowfall.py

--- lab11/1_snowfall.py
+++ lab11/1_snowfall.py
@@ -1,32 +1,38 @@
 import simple_draw as sd
 
+
 class Snowflake:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.length = 0
+        self.factor_a = 0
+        self.factor_b = 0
+        self.factor_c = 0
+
     def create(self):
-        self.x = sd.random_number(0, sd.resolution[0])  # Random starting X coordinate
-        self.y = sd.resolution[1]  # Starting Y coordinate (at the bottom of the screen)
-        self.length = sd.random_number(10, 40)  # Length of the snowflake
-        self.factor_a = sd.random_number(1, 10) / 10  # Horizontal amplitude factor
-        self.factor_b = sd.random_number(1, 10) / 10  # Vertical amplitude factor
-        self.factor_c = sd.random_number(10, 60)  # Speed factor
+        self.x = sd.random_number(0, sd.resolution[0])  # Случайная начальная координата X
+        self.y = sd.resolution[1]  # Начальная координата Y (внизу экрана)
+        self.length = sd.random_number(10, 40)  # Длина снежинки
+        self.factor_a = sd.random_number(1, 10) / 10  # Горизонтальный фактор амплитуды
+        self.factor_b = sd.random_number(1, 10) / 10  # Вертикальный фактор амплитуды
+        self.factor_c = sd.random_number(10, 60)  # Фактор скорости
 
     def clear_previous_picture(self):
-        sd.start_drawing()
         point = sd.get_point(self.x, self.y)
-        sd.snowflake_center(point, self.length, sd.background_color)
-        sd.finish_drawing()
+        sd.snowflake(point, self.length, sd.background_color)
 
     def move(self):
-        self.x += sd.random_number(-2, 2)  # Add some horizontal variation
-        self.y -= self.factor_c  # Move the snowflake upwards
+        self.x += sd.random_number(-2, 2)  # Добавляем некоторое горизонтальное отклонение
+        self.y -= self.factor_c  # Перемещаем снежинку вверх
 
     def draw(self):
         point = sd.get_point(self.x, self.y)
-        sd.start_drawing()
-        sd.snowflake_center(point, self.length, sd.COLOR_WHITE)
-        sd.finish_drawing()
+        sd.snowflake(point, self.length, sd.COLOR_WHITE)
 
     def can_fall(self):
-        return self.y > 0  # Check if the snowflake is still within the screen bounds
+        return self.y > 0  # Проверяем, находится ли снежинка в пределах экрана
+
 
 flake = Snowflake()
 
@@ -40,4 +46,5 @@ while True:
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
+
 sd.pause()
